32133067420ff11e3d6e90050ec54f59420077c6
Divi202 <divya@yahoo.com>
2025-06-11 17:04:31 +0530

    Project setup & Shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["pretttier"],
+    rules: {
+      "prettier/prettier": "error",
+      "react/no-escape-entities": "off", // Next.js does not require React to be in scope
+    },
+  }),
 ];
 
 export default eslintConfig;
